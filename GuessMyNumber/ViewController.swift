78e44e13f8a30e55829c0dc4609bd0eead8308e4
Brian Foutty <brian@swiftteacher.org>
2023-05-09 11:17:08 -0400

    Initial Commit

---
+++ GuessMyNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessMyNumber
+//
+//  Created by Brian Foutty on 5/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
