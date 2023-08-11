a9b7c3dca8a1682a940ec60ad6ba09eb4837179f
Brian Foutty <brian@swiftteacher.org>
2023-08-11 11:54:23 -0400

    Game has basic functionality. Still need to clear after submit, create playAgain function, and set labels for first load.

--- GuessMyNumber/ViewController.swift
+++ GuessMyNumber/ViewController.swift
@@ -8,26 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // #1  - set up UI in Main
     
     // MARK: - IB Outlets
+    // #2 - connect outlets in Assistant Editor
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var hintLabel: UILabel!
     @IBOutlet weak var numberTextField: UITextField!
     
     // MARK: - Instance Properties
-
+    // #4 - create these three properties
+    var guesses = 0
+    var randomNumber = 50
+    var playerGuess = ""
     
     
     // MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        generateRandomNumber()
     }
     
     // MARK: - IB Actions
+    // #4 - connect submit button in Assistant Editor
     @IBAction func submitButtonTapped(_ sender: Any) {
-        
+        checkNumber()
     }
     
-    // MARK: - Segues
+    // MARK: - Instance Methods
+    // generate a random number
+    func generateRandomNumber() {
+        randomNumber = Int.random(in: 1...100)
+        print(randomNumber)
+    }
+    
+    func checkNumber() {
+        let playerGuess = Int(numberTextField.text ?? "50")
+            
+            if playerGuess == randomNumber {
+                hintLabel.text = "EXCELLENT! You guessed the number!🥳"
+                scoreLabel.text = "You guessed the number in \(guesses) guesses."
+            } else if playerGuess ?? 50 > randomNumber {
+                guesses += 1
+                hintLabel.text = "⬇️ Your guess is greater than the number."
+            } else {
+                guesses += 1
+                hintLabel.text? = "⬆️ Your guess is less than the number."
+            }
+    }
+    
 
 }
 
