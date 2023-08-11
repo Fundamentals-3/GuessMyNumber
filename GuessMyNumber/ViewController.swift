2aa2c490cfd9cd09a6e893abe7aea1e88a4cc1ad
Brian Foutty <brian@swiftteacher.org>
2023-08-11 13:29:26 -0400

    Set some labels. Safely unwrapping optionals. Need a reset function.

--- GuessMyNumber/ViewController.swift
+++ GuessMyNumber/ViewController.swift
@@ -26,38 +26,57 @@ class ViewController: UIViewController {
     // MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
+        scoreLabel.text = ""
+        hintLabel.text = ""
         
+        // #6 - call generateRandomNumber to generate the first random number
         generateRandomNumber()
     }
     
     // MARK: - IB Actions
     // #4 - connect submit button in Assistant Editor
     @IBAction func submitButtonTapped(_ sender: Any) {
+        //
         checkNumber()
     }
     
     // MARK: - Instance Methods
-    // generate a random number
+    // #5 - generate a random number
     func generateRandomNumber() {
         randomNumber = Int.random(in: 1...100)
         print(randomNumber)
     }
     
+    func getGuessedNumber() -> Int? {
+        guard let playerGuessText = self.numberTextField.text,
+              let playerGuess = Int(playerGuessText) else
+        {
+            //print("There is no number entered.")
+            hintLabel.textColor = .red
+            hintLabel.text = "Please enter a number."
+            scoreLabel.text = ""
+            return nil }
+        
+        return playerGuess
+    }
+    
     func checkNumber() {
-        let playerGuess = Int(numberTextField.text ?? "50")
+        //let playerGuess = Int(numberTextField.text ?? "50")
+        if let playerGuess = getGuessedNumber() {
             
             if playerGuess == randomNumber {
+                hintLabel.textColor = .magenta
                 hintLabel.text = "EXCELLENT! You guessed the number!🥳"
                 scoreLabel.text = "You guessed the number in \(guesses) guesses."
-            } else if playerGuess ?? 50 > randomNumber {
+            } else if playerGuess > randomNumber {
                 guesses += 1
                 hintLabel.text = "⬇️ Your guess is greater than the number."
             } else {
                 guesses += 1
                 hintLabel.text? = "⬆️ Your guess is less than the number."
             }
+        }
     }
-    
 
 }
 
